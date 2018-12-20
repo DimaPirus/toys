a6b95c048b37baf3d3c5159d27456a0979ae1bef
dimapirus <dimapirus@gmail.com>
2018-12-20 15:29:30 +0200

    ...

--- js/js.js
+++ js/js.js
@@ -32,3 +32,37 @@ responsive: [
     // instead of a settings object
   ]
 });
+$('.sl-brand').slick({
+dots: true,
+infinite: false,
+speed: 1000,
+slidesToShow: 6,
+slidesToScroll: 6,
+swipe: true,
+TouchMove: true,
+touchThreshold: 4,
+verticalSwiping: true,
+swipeToSlide: true,
+//сколько слайдов за раз перелистнется
+responsive: [
+    {
+      breakpoint: 750,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 370,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
